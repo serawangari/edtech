c8e4236e3803ef494ed00d5c3ebfbb977e21155b
serawangari <148107266+serawangari@users.noreply.github.com>
2025-09-30 16:03:42 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,26 @@
+// middleware.ts
 import createMiddleware from 'next-intl/middleware';
+import { NextResponse } from 'next/server';
 
-export default createMiddleware({
-  locales: ['en'],
-  defaultLocale: 'en',
-  // no prefix when there's only one locale
-  localePrefix: 'never',
-});
+import intlConfig from './next-intl.config.mjs';
+
+const intlMiddleware = createMiddleware(intlConfig);
+
+export default function middleware(req: Request) {
+  const url = new URL(req.url);
+
+  // If no locale in the pathname, redirect to defaultLocale
+  if (url.pathname === '/') {
+    return NextResponse.redirect(new URL('/en', req.url));
+  }
+
+  return intlMiddleware(req);
+}
 
 export const config = {
   matcher: ['/', '/(en)/:path*'],
 };
+
 
 
 
