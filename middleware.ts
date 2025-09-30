23ca855563db8a2966578ccac57b0e9c637f32c6
serawangari <148107266+serawangari@users.noreply.github.com>
2025-09-30 09:37:26 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
+// middleware.ts
 import createMiddleware from 'next-intl/middleware';
-import {locales, defaultLocale, localePrefix} from './next-intl.config';
+import intl from './next-intl.config';
 
-export default createMiddleware({locales, defaultLocale, localePrefix});
+export default createMiddleware(intl);
 
 export const config = {
-  // match root and /en/...; expand later if you add more locales
-  matcher: ['/', '/(en)/:path*']
+  matcher: ['/', '/(en)/:path*'] // extend when adding more locales
 };
