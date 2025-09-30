bc66b6a4c8a95e1e91a7fdb063b17786cd78751b
serawangari <148107266+serawangari@users.noreply.github.com>
2025-09-30 15:59:53 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 // app/layout.tsx
-import Script from 'next/script';
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const SEGMENT_KEY = process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY;
-
   return (
     <html lang="en">
-      <body>
-        {children}
-
-        
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
