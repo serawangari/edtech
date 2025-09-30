5145eb07f36012dd3c552c00c14b8652a4b6b4f5
serawangari <148107266+serawangari@users.noreply.github.com>
2025-09-30 09:38:07 +0300

    Update next-intl.config.mjs

--- next-intl.config.mjs
+++ next-intl.config.mjs
@@ -1,8 +1,8 @@
+// next.config.mjs
 import createNextIntlPlugin from 'next-intl/plugin';
-
-// IMPORTANT: path must point to /i18n/request.ts
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 export default withNextIntl({
   reactStrictMode: true
 });
+
